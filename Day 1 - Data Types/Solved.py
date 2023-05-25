22e15e141c594b51c18ac9f847a676667b9fee14
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-25 13:51:57 -0300

    Printing sum and concatenating str variables.

--- Day 1 - Data Types/Solved.py
+++ Day 1 - Data Types/Solved.py
@@ -10,8 +10,9 @@ a = int(input())
 b = float(input())
 c = str(input())
 # Print the sum of both integer variables on a new line.
-
+print(i + a)
 # Print the sum of the double variables on a new line.
-
+print(d + b)
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
+print(s + c)
