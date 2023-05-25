b3b77b072cc0e86475a9b3b7944da28cbca5561a
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-25 13:50:55 -0300

    Reading and saving values to variables.

--- Day 1 - Data Types/Solved.py
+++ Day 1 - Data Types/Solved.py
@@ -6,7 +6,9 @@ a = 12
 b = 4.0
 c = 'is the best place to learn and practice coding!'
 # Read and save an integer, double, and String to your variables.
-
+a = int(input())
+b = float(input())
+c = str(input())
 # Print the sum of both integer variables on a new line.
 
 # Print the sum of the double variables on a new line.
