b1fee57dfc9a0eaa96ed4a4aba3888d35caeb8b2
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-25 14:20:40 -0300

    solved challange

--- Day 2 - Operators/Solved.py
+++ Day 2 - Operators/Solved.py
@@ -13,11 +13,9 @@ import sys
 #  1. DOUBLE meal_cost
 #  2. INTEGER tip_percent
 #  3. INTEGER tax_percent
-#
 
 
-def solve(meal_cost, tip_percent, tax_percent):
-    # Write your code here
+def resolve(meal_cost, tip_percent, tax_percent):
     tip = (meal_cost / 100) * tip_percent
     tax = (tax_percent / 100) * meal_cost
     total_cost = meal_cost + tip + tax
@@ -26,11 +24,8 @@ def solve(meal_cost, tip_percent, tax_percent):
     print('Round value:', round(total_cost))
 
 
-if __name__ == '__main__':
-    meal_cost = float(input('Insert the meal price: ').strip())
+meal_cost = float(input('Insert the meal price: '))
+tip_percent = int(input('Insert the tip percent: '))
+tax_percent = int(input('Insert the tax percent: '))
 
-    tip_percent = int(input('Insert the tip percent: ').strip())
-
-    tax_percent = int(input('Insert the tax percent: ').strip())
-
-    solve(meal_cost, tip_percent, tax_percent)
+resolve(meal_cost, tip_percent, tax_percent)
