cc3245b8e6d6c6906e6a5ceadf0405129a6a5ec4
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-25 14:16:06 -0300

    Creating some changes to improve the code.

--- Day 2 - Operators/Solved.py
+++ Day 2 - Operators/Solved.py
@@ -18,13 +18,19 @@ import sys
 
 def solve(meal_cost, tip_percent, tax_percent):
     # Write your code here
+    tip = (meal_cost / 100) * tip_percent
+    tax = (tax_percent / 100) * meal_cost
+    total_cost = meal_cost + tip + tax
+    print('\nTotal cost:', total_cost)
+
+    print('Round value:', round(total_cost))
 
 
 if __name__ == '__main__':
-    meal_cost = float(input().strip())
+    meal_cost = float(input('Insert the meal price: ').strip())
 
-    tip_percent = int(input().strip())
+    tip_percent = int(input('Insert the tip percent: ').strip())
 
-    tax_percent = int(input().strip())
+    tax_percent = int(input('Insert the tax percent: ').strip())
 
     solve(meal_cost, tip_percent, tax_percent)
