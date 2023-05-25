c33022df4ea828401048588e1522e5e5687287a8
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-25 13:44:23 -0300

    Printing 'hello, world.' message and 'input_string' content.

--- Day 0 - Hello, World/Solved.py
+++ Day 0 - Hello, World/Solved.py
@@ -2,5 +2,6 @@
 input_string = input()
 
 # Print a string literal saying "Hello, World." to stdout.
-
+print("Hello, world.")
 # TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
