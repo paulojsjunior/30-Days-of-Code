3f0fc2b62d480471aba33c26b96cd45d3189c2b3
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-25 15:07:34 -0300

    Conditial to check if age = 0 or not.

--- Day 4 - Class vs Instance/Solved.py
+++ Day 4 - Class vs Instance/Solved.py
@@ -1,6 +1,11 @@
 class Person:
     def __init__(self, initialAge):
         # Add some more code to run some checks on initialAge
+        if initialAge < 0:
+            self.age = 0
+            print('Age is not valid, setting age to 0.')
+        else:
+            self.age = initialAge
 
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
