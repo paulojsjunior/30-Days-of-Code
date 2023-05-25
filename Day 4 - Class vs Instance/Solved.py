cc98add9ca08117dae74785f323ac0af5e895ca0
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-25 15:08:51 -0300

    We've set another conditials to return some informations depending of the age value.

--- Day 4 - Class vs Instance/Solved.py
+++ Day 4 - Class vs Instance/Solved.py
@@ -9,6 +9,12 @@ class Person:
 
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
+        if self.age < 13:
+            print('You are young.')
+        elif self.age >= 13 and self.age < 18:
+            print('You are a teenager.')
+        else:
+            print('You are old.')
 
     def yearPasses(self):
         # Increment the age of the person in here
