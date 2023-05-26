1997b38a458b1d1ffc3e4cd954d3c0d730202353
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-26 16:37:01 -0300

    Prints were used to define the separator between output values

--- Day 6 - Let's Review/Solved.py
+++ Day 6 - Let's Review/Solved.py
@@ -8,6 +8,10 @@ for x in range(0, N):
         if i % 2 == 0:
             print(S[i], end='')
 
+    print(" ", end='')
+
     for i in range(0, len(S)):
         if i % 2 != 0:
             print(S[i], end='')
+
+    print("")
