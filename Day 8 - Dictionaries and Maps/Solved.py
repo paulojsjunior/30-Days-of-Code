a0e32bca6a9df280b45574570c480c451e958754
Paulo Júnior <paulojsjunior9@gmail.com>
2023-05-29 15:28:20 -0300

    Another loop to check if a name contact is in the phone book

--- Day 8 - Dictionaries and Maps/Solved.py
+++ Day 8 - Dictionaries and Maps/Solved.py
@@ -4,3 +4,11 @@ phoneBook = {}
 for i in range(N):
     contactName, phoneNumber = input().split(' ')
     phoneBook[contactName] = phoneNumber
+
+for j in range(N):
+    name = input()
+
+    if name in phoneBook.keys():
+        print(f'{name}={phoneBook[phoneNumber]}')
+    else:
+        print('Not found.')
